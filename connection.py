cc9dc78328e92bb4e3c5370f93e2a3a701e616cb
fjukh7872 <hayimdavid6@gmail.com>
2024-09-29 15:23:52 +0300

    NoMoreUgly

---
+++ connection.py
@@ -0,0 +1,45 @@
+from __future__ import annotations
+import socket
+import struct
+PARTIAL_DATA = "The connenction ended before all of the data was received. "
+
+
+
+class Connection():
+    def __init__(self, connection: socket.socket):
+        self.connection = connection
+
+    def __repr__(self) -> str:
+        return f"Connection from {self.connection.getsockname()} to {self.connection.getpeername()}"
+    
+    def send_message(self, message: bytes):
+        self.connection.sendall(message)
+    
+    def receive_message(self) -> str:
+        length = struct.unpack("<I", self.connection.recv(4))[0]
+        message = b''
+        while len(message) < length:
+            data = self.connection.recv(4096)
+            if data:
+                message += data
+            else:
+                raise Exception(PARTIAL_DATA)
+        message = message.decode()
+        return message
+    
+    @classmethod
+    def connect(cls, host: str, port: int) -> Connection:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serv:
+            serv.bind((host, port))
+            serv.listen()
+            conn = serv.accept()[0]
+        return conn
+    
+    def close(self):
+        self.connection.close()
+    
+    def __enter__(self) -> Connection:
+        return self
+    
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        self.close()
